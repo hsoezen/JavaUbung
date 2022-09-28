9bae89a30075ca7c1fe366aadf25033798b41602
hsoezen <Huesamettin.Soezen@fincon.eu>
2022-09-28 11:31:12 +0200

    Java

---
+++ src/test/java/JavaÜben.java
@@ -0,0 +1,11 @@
+public class JavaÜben {
+
+    //Finden Min und Max Werte zwischen x und y
+    public static void main(String[] args) {
+        int x = 45;
+        int y = 23;
+
+        System.out.println(Math.max(x,y));
+        System.out.println(Math.min(x,y));
+    }
+}
